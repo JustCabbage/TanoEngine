b44bed0fd4d32f2c87fd069675dfcd52274537d1
양배추 <47952062+JustCabbage@users.noreply.github.com>
2024-09-08 23:19:58 +0100

    Fixed std::views::enumerator GCC issue

--- tests/TestScene.cpp
+++ tests/TestScene.cpp
@@ -17,8 +17,7 @@ namespace Tano
 
     void TestScene::Update(std::uint32_t DeltaTime)
     {
-        /*
-        for (const auto [Index, Entity] : std::views::enumerate(m_EntityContainer.GetEntities()))
+        for (const auto& Entity : m_EntityContainer.GetEntities())
         {
             if (Entity.HasComponent<TransformComponent>())
             {
@@ -36,7 +35,6 @@ namespace Tano
                 // Do something with Collide component
             }
         }
-        */
     }
 
     void TestScene::Render(sf::RenderWindow& Window)
